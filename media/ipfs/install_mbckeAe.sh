3dbded0379bc4e86a4e6816d0c18ff965edef751
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2019-08-19 20:06:16 +0530

    Removed media and static folder

--- media/ipfs/install_mbckeAe.sh
+++
@@ -1,7 +0,0 @@
-HOSTNAME=0.0.0.0
-
-# Nvm needs to be installed for this. Refer ../NodeJS setup/install-nvm.sh for instructions
-. ~/.nvm/nvm.sh # Allows us to use nvm commands
-
-#Angular Cli must be installed for the following command
-ng serve 
