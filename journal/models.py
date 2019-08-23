8c4933e5896a54b19d2cb921b731d5dd36fc796c
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2019-08-23 17:56:24 +0530

    Add a model GithubResearchObject

--- journal/models.py
+++ journal/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class UploadedResearchObject(models.Model):
     oricid = models.CharField(max_length=100)
     uploadedfile = models.FileField(upload_to='ipfs')
+
+class GithubResearchObject(models.Model):
+    oricid = models.CharField(max_length=100)
+    downloadedrepozip = models.FileField(upload_to='github')
