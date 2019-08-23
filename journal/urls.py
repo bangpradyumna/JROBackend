7cbd588c58fc69e64c09a586f11a8420de073816
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2019-08-23 20:10:26 +0530

    Register url for newly created addgithubrepo endpoint.

--- journal/urls.py
+++ journal/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from journal.views.ipfs import addfile
+from journal.views.github import addgithubrepo
 
 urlpatterns = [ 
-    path('ipfs/addfile/', addfile.addFile, name='AddFile'),
+    path('ipfs/addfile/', addfile.addFile, name='Add File'),
+    path('github/addrepo/', addgithubrepo.addGithubRepo, name='Add Github Repo'),
 ]
