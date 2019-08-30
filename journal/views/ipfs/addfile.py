cc51792ccd459e4fb51ce061096f678a148959aa
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2019-08-30 22:41:43 +0530

    Replaced the hardcoded url values

--- journal/views/ipfs/addfile.py
+++ journal/views/ipfs/addfile.py
@@ -7,6 +7,7 @@ from django.core.files import File
 from journal.models import UploadedResearchObject
 import ipfshttpclient
 import requests
+from JROBackend.keyconfig import COMPOSER_REST_URL, IPFS_ADDRESS
 
 @csrf_exempt
 @api_view(['POST'])
@@ -20,14 +21,14 @@ def addFile(request):
     )
 
     try:
-        client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
+        client = ipfshttpclient.connect(IPFS_ADDRESS)
         filepath = ro.uploadedfile.path
         res = client.add(filepath)
         print("The hash of file is : ",res['Hash'])
         researcher = "resource:org.jro.Researcher#"+str(ro.oricid)
         rojid="resource:org.jro.ROJ#"+ str(res['Hash'])
         print("\n Adding the research object to the blockchain")
-        r = requests.post('http://localhost:5002/api/Add', data = { "$class": "org.jro.Add", "rojId": rojid, "node": res['Hash'], "creator": researcher })
+        r = requests.post(COMPOSER_REST_URL+'/api/Add', data = { "$class": "org.jro.Add", "rojId": rojid, "node": res['Hash'], "creator": researcher })
         print(r.content)
         if r.status_code==200:
             print("\n Success")
